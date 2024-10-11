5fd54a09681e4307bd8db7d8bca44dde9d0ef7e9
Lloyd Allen <lloydallen597@gmail.com>
2024-10-10 23:55:07 -0500

    Making a Score Keeper

--- Quiz Mastermind/Assets/Scripts/Quiz.cs
+++ Quiz Mastermind/Assets/Scripts/Quiz.cs
@@ -23,10 +23,16 @@ public class Quiz : MonoBehaviour
     [SerializeField] Image timerImage;
     Timer timer;
 
+    [Header("Scoring")]
+    [SerializeField] TextMeshProUGUI scoreText;
+    ScoreKeeper scoreKeeper;
+
+
     void Start()
     {
         timer = FindObjectOfType<Timer>();
         GetInitialQuestion(); //had to add to remove NULL REF EXCEPTION
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
     void GetInitialQuestion() //had to add to remove NULL REF EXCEPTION
@@ -57,6 +63,7 @@ public class Quiz : MonoBehaviour
         DisplayAnswer(index);
         SetButtonState(false);
         timer.CancelTimer();
+        scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
     }
 
     void DisplayAnswer(int index)
@@ -68,6 +75,7 @@ public class Quiz : MonoBehaviour
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
         }
         else
         {
@@ -87,6 +95,7 @@ public class Quiz : MonoBehaviour
             SetDefaultButtonSprites();
             GetRandomQuestion();
             DisplayQuestion();
+            scoreKeeper.IncrementQuestionsSeen();
         }
     }
 
